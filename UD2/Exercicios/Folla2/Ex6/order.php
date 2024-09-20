fb7bad0008f997cbbccd3fb8fa72cface8608f43
Andres Caruncho <a23andrescf@iessanclemente.net>
2024-09-20 17:23:59 +0200

    new folla 4

--- UD2/Exercicios/Folla2/Ex6/order.php
+++ UD2/Exercicios/Folla2/Ex6/order.php
@@ -15,39 +15,13 @@
             $num3 = $_GET['num3'];
 
             if (is_numeric($num1) && is_numeric($num2) && is_numeric($num3)) {
-                if ($num1 > $num2) {
-                    $maior = $num1;
-                    $menor = $num2;
-                    if ($num3 > $maior) {
-                        $maior = $num3;
-                        $medio = $num1;
-                    } else {
-                        if ($num3 < $menor) {
-                            $medio = $num2;
-                            $menor = $num3;
-                        } else {
-                            $medio = $num3;
-                        }
-                    }
-                } else {
-                    $maior = $num2;
-                    $menor = $num1;
-                    if ($num3 > $maior) {
-                        $maior = $num3;
-                        $medio = $num2;
-                    } else {
-                        if ($num3 < $menor) {
-                            $medio = $num1;
-                            $menor = $num3;
-                        } else {
-                            $medio = $num3;
-                        }
-                    }
-                }
 
-                echo "<p>".$maior."</p>";
-                echo "<p>".$medio."</p>";
-                echo "<p>".$menor."</p>";
+                $maior = max($num1, $num2, $num3);
+                $menor = min($num1, $num2, $num3);
+
+                echo "<p>O maior é ".$maior."</p>";
+                echo "<p>O menor é".$menor."</p>";
+
             } else {
                 echo "Algo saliu mal...";
             }
