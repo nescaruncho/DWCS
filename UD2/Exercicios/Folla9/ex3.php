1d1b373e9703f54952b9b064531bc018035d8de6
nescaruncho <nescaruncho@gmail.com>
2024-10-08 19:02:55 +0200

    hoja 9 hasta 5 incompleto

---
+++ UD2/Exercicios/Folla9/ex3.php
@@ -0,0 +1,26 @@
+<?php
+
+function compararClaves($a, $b) {
+    return strcmp($a, $b);
+}
+
+$datos = array('f' => 4, 'g' => 8, 'a' => 1, 'b' => -10);
+uksort($datos, 'compararClaves');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Datos</h1>
+    <ul>
+        <?php foreach ($datos as $clave => $valor) : ?>
+            <li><?= $clave ?>: <?= $valor ?></li>
+        <?php endforeach; ?>
+</body>
+</html>
