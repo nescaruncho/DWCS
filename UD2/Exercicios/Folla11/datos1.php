8a0fe3425bdca34fa4130441990349c90e7503c0
nescaruncho <nescaruncho@gmail.com>
2024-10-14 14:43:30 +0200

    nuevas hojas de ejercicios sin resolver

---
+++ UD2/Exercicios/Folla11/datos1.php
@@ -0,0 +1,14 @@
+﻿
+<?php
+$pelis=array("Ciudadano Kane"=>array("Orson Welles",119),
+			 "Casablanca"=>array("Michael Curtiz",102),
+			 "El Padrino"=>array("Francis Ford Coppola",175),
+			 "Lo que el viento se llevó"=>array("Victor Fleming",224),
+			 "Lawrence de Arabia"=>array("David Lean",217),
+			 "El mago de Oz"=>array("Victor Fleming",101),
+			 "El graduado"=>array("Mike Nichols",105),
+			 "La ley del silencio"=>array("Elia Kazan",108),
+			 "La lista de Schindler"=>array("Steven Spielberg",195),
+			 "Cantando bajo la lluvia"=>array("Stanley Donen-Gene Kelly",99));
+
+
